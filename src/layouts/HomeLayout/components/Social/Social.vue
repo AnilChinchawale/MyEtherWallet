fd888e461036a0da914afbe5be56fa3acdb45346
AnilChinchawale <anil24593@gmail.com>
2021-02-16 11:18:09 +0530

    Atauro's Original Wallet

--- src/layouts/HomeLayout/components/Social/Social.vue
+++ src/layouts/HomeLayout/components/Social/Social.vue
@@ -1,31 +1,4 @@
-<template>
-  <div class="social">
-    <div class="wrap">
-      <div class="page-container">
-        <div class="content">
-          <div class="title">
-            <h2>{{ $t('home.social.title') }}</h2>
-          </div>
-          <div class="icons">
-            <div v-for="(link, index) in links" :key="link.name + index">
-              <a
-                :href="link.to"
-                :aria-label="link.name"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div :class="link.name">
-                  <img :src="link.img" alt />
-                  <p class="link-name">{{ link.name | capitalize }}</p>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
+
 
 <script>
 import github from '@/assets/images/icons/github.png';
